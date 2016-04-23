1e4bbf291bae3aabd6cef25e50a871335a2f401f
Adek2kk <adam0493@o2.pl>
2016-04-23 07:15:47 -0700

    Prepare import view and lib

---
+++ ImportData/ImportCSV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ConnDBlib;
+
+namespace ImportData
+{
+    public static class ImportCSV
+    {
+        public static void importTable(string prefix, string pathFile)
+        {
+
+        }
+    }
+}
---
+++ IntervalApp/MainUserControls/Import.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntervalApp.MainUserControls
+{
+    /// <summary>
+    /// Interaction logic for Import.xaml
+    /// </summary>
+    public partial class Import : UserControl
+    {
+        public Import()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFind_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            dialog.DefaultExt = ".csv";
+            dialog.Filter = "CSV Files (*.csv)|*.csv";
+            Nullable<bool> result = dialog.ShowDialog();
+
+            if (result == true)
+                TxtSource.Text = dialog.FileName.ToString();
+        }
+
+        private void BtnImport_Click(object sender, RoutedEventArgs e)
+        {
+            string SourcePath;
+            SourcePath = TxtSource.Text;
+            if (File.Exists(SourcePath))
+            {
+                if (SourcePath.Substring(SourcePath.Length - 4, 4) == ".csv")
+                {
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Type of file must be a .csv!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("File doesn't exists!");
+            }
+        }
+    }
+}
