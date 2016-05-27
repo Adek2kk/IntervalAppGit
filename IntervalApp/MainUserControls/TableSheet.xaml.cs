1271e92d6ab60b576b46cd33ce2ab9aa0815d808
Sakadin <p.przybylowski@Onet.eu>
2016-05-27 02:56:58 -0700

    Update pomocnika

--- IntervalApp/MainUserControls/TableSheet.xaml.cs
+++ IntervalApp/MainUserControls/TableSheet.xaml.cs
@@ -30,9 +30,10 @@ namespace IntervalApp.MainUserControls
         {
             List<string> listItemsFac = new List<string>();
             List<string> listItemsDim = new List<string>();
+            List<string> listItemsFun = new List<string>();
             DataSet dimensions = DimensionHandler.getDimensions(Application.Current.Resources["ProjectPrefix"].ToString());
             DataSet facts = FactHandler.getFacts(Application.Current.Resources["ProjectPrefix"].ToString());
-
+           DataSet functions = FunctionHandler.getFunctions(Application.Current.Resources["ProjectPrefix"].ToString());
             foreach (DataRow row in dimensions.Tables["result"].Rows)
             {
                 listItemsDim.Add(row[0].ToString());
@@ -41,9 +42,39 @@ namespace IntervalApp.MainUserControls
             {
                 listItemsFac.Add(row[0].ToString());
             }
+            foreach (DataRow row in functions.Tables["result"].Rows)
+           {
+                listItemsFun.Add(row[0].ToString());
+           }
 
             listViewFacts.ItemsSource = listItemsFac;
             listViewDim.ItemsSource = listItemsDim;
+            listViewFun.ItemsSource = listItemsFun;
+        }
+        public List<string> columnList(string tableName)
+        {
+            List<string> listItems = new List<string>();
+            DataSet testowy = DimensionHandler.getDimensionColumns(tableName);
+            foreach (DataRow row in testowy.Tables["result"].Rows)
+            {
+                listItems.Add(row[0].ToString());
+            }
+            return listItems;
+        }
+
+        private void listViewFacts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listViewFactsCol.ItemsSource = columnList(listViewFacts.SelectedItem.ToString());
+        }
+
+        private void listViewDim_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listViewDimCol.ItemsSource = columnList(listViewDim.SelectedItem.ToString());
+        }
+
+        private void listViewFun_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            listViewFunCol.ItemsSource = columnList(listViewFun.SelectedItem.ToString());
         }
     }
 }
